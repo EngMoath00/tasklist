670c1e5b6e589f25dda5f5d56ad0629d1f1a6803
moath mohammed <engmoath00@gmail.com>
2022-03-26 14:59:26 +0300

    lect2 update all

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
+|---------------------------------------------------------------------  -----
 | Web Routes
 |--------------------------------------------------------------------------
 |
@@ -13,6 +13,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return "WElcome Eng moath this is web site";
+// });
+
+Route::get('/about', function () {
+    $name = 'moath mohammed';
+    $age = 23;
+
+    //3 ways to send data from database or aother to view via router
+    // return view('aboutdata', ['name' => $name, 'age' => $age]);
+    // return view('aboutdata', compact('name', 'age'));
+    // return view('aboutdata')->with('name',$name)->with('age',$age);
+
 });
+
+
+//Route Parameters
+//1- Required Parameters
+Route::get('/product/{id}', function ($id) {
+
+    return 'product No:' . $id;
+})->name('productId');
+
+//2- Optional Parameters
+// Route::get('/product/{id?}', function ($id = 0) {
+
+//     return 'product No:' . $id;
+// });
